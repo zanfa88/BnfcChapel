6b64ae74a18cd7fe8310e1d0dbb4b404954f5771
David <dasko88>
2015-12-31 18:24:51 +0100

    grammatica con real -mod test

--- chap-simple-5/build.sh
+++ chap-simple-5/build.sh
@@ -34,14 +34,12 @@ echo "var a:real=1.0,b:int=4;b=20.0+1.0;a=2.0+2.3;" | ./Testchapel
 echo "-----------------" 
 echo "var a:real=1.0,b:int=4;b=20.0+1.0;a=2.0+2.3;" | ./Testchapel 
 echo "var a:real=1.0,b:int=4;b=20.0+1.0;a=2.0+2.3;" | ./Testchapel 
-echo "if (q<=2) then a=3; else c=1;" | ./Testchapel 
+echo "var q:int=2;if (q<=2) then q=3; else q=1;" | ./Testchapel 
 echo "if (q>2) {a=3;c=3;} else {c=8;c=11;}" | ./Testchapel 
 echo "select (a) {when 1 do a=3;when 2 {a=3;b=4;} otherwise {c=5;d=3;}}" | ./Testchapel 
-echo "while (c>=3) do a=2;" | ./Testchapel 
-echo "while (c>=3) {a=2;b=3;}" | ./Testchapel 
-echo "do {a=3;b=4;} while (a==true);" | ./Testchapel 
-echo "for i in 1..10 do {a=3;}" | ./Testchapel 
-echo "for j in 1..10 {a=3;a=2;a=6;};" | ./Testchapel 
-echo "for a in 1..220 {if (a==3) then break; else continue; a=4;}" | ./Testchapel 
+echo "var c:int=2;while (c>=3) do c=2;" | ./Testchapel 
+echo "var c:int=2;while (c>=3) {c=2;c=3;}" | ./Testchapel 
+echo "var a:int=2;for a in 1..10 do {a=3;}" | ./Testchapel 
+echo "var a:int=2;for a in 1..220 {if (a==3) then break; else continue; a=4;}" | ./Testchapel 
 echo "writeInt(4);" | ./Testchapel 
 echo "function mainA (a:int) { a=3; c=z; } a=5; c=7; function mainB () { 	a=3; 	c=z; } function mainC (a:int,b:int) { 	a=3; 	c=z; } mainC(c,2); mainB(); mainA(a==true); " | ./Testchapel 
